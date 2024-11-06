24610468bc73bbf380186bcf22204428f538cd06
senbet22 <152603405+senbet22@users.noreply.github.com>
2024-11-06 18:58:41 +0700

    logout updte

--- src/js/ui/auth/logout.js
+++ src/js/ui/auth/logout.js
@@ -2,4 +2,7 @@
  * This function should log the user out by removing aproppriate user data from the browser.
  */
 
-export function onLogout() {}
+export function onLogout() {
+  localStorage.removeItem("adminUser", JSON.stringify(adminUser));
+  localStorage.removeItem("token", data.data.accessToken);
+}
