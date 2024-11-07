f5c374193e3a1fd7bf8a6c5d909e16d83b650947
senbet22 <152603405+senbet22@users.noreply.github.com>
2024-11-07 15:32:32 +0700

    logout Update

--- src/js/ui/global/logout.js
+++ src/js/ui/global/logout.js
@@ -1,6 +1,10 @@
+
 /**
  * Functions you attach to logout events that calls ui/auth/logout function
  */
+
+import { onLogout } from './logout';
+
 export function setLogoutListener() {
   const logoutButton = document.getElementById("logout-button");
 
