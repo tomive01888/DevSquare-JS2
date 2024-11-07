63a0be5f961355c984dba8edbc5e85d65dab2b7b
tomive_NOROFF <152199718+tomive01888@users.noreply.github.com>
2024-11-07 15:59:16 +0100

    --Correct few styles and buttons for create and edit posts.
    -- Made fetchUrl in backend of create and edit.
    
    --Changed the url params in homePostsBuilder

--- src/js/ui/post/create.js
+++ src/js/ui/post/create.js
@@ -5,25 +5,24 @@
 import { createPost } from "../../api/post/create";
 
 export async function onCreatePost(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const title = event.target.title.value.trim();
-    const body = event.target.body.value.trim();
-    const tags = event.target.tags.value ? event.target.tags.value.split(",").map((tag) => tag.trim()) : [];
-    const media = event.target.mediaUrl.value ? { url: event.target.mediaUrl.value, alt: event.target.mediaAlt.value || "" } : null;
+  const title = event.target.title.value.trim();
+  const body = event.target.body.value.trim();
+  const tags = event.target.tags.value ? event.target.tags.value.split(",").map((tag) => tag.trim()) : [];
+  const media = event.target.mediaUrl.value
+    ? { url: event.target.mediaUrl.value, alt: event.target.mediaAlt.value || "" }
+    : null;
 
-    try {
-        const response = await createPost({title, body, tags, media})
+  try {
+    const response = await createPost({ title, body, tags, media });
 
-        if(response){
-            alert("Successfully created a new post")
-        } else {
-            alert("Something went wrong, failed to create post.")
-        }
-    } catch (error) {
-        console.error("Error creating post:", error)
-        
+    if (response) {
+      alert("Successfully created a new post");
+    } else {
+      alert("Something went wrong, failed to create post.");
     }
-
+  } catch (error) {
+    console.error("Error creating post:", error);
+  }
 }
-
--- src/js/ui/post/update.js
+++ src/js/ui/post/update.js
@@ -5,24 +5,24 @@
 import { updatePost } from "../../api/post/update";
 
 export async function onUpdatePost(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const title = event.target.title.value.trim();
-    const body = event.target.body.value.trim();
-    const tags = event.target.tags.value ? event.target.tags.value.split(",").map((tag) => tag.trim()) : [];
-    const media = event.target.mediaUrl.value ? { url: event.target.mediaUrl.value, alt: event.target.mediaAlt.value || "" } : null;
+  const title = event.target.title.value.trim();
+  const body = event.target.body.value.trim();
+  const tags = event.target.tags.value ? event.target.tags.value.split(",").map((tag) => tag.trim()) : [];
+  const media = event.target.mediaUrl.value
+    ? { url: event.target.mediaUrl.value, alt: event.target.mediaAlt.value || "" }
+    : null;
 
-    try {
-        const response = await updatePost({title, body, tags, media})
+  try {
+    const response = await updatePost({ title, body, tags, media });
 
-        if(response){
-            alert("Successfully update a new post")
-        } else {
-            alert("Something went wrong, failed to update post.")
-        }
-    } catch (error) {
-        console.error("Error updating post:", error)
-        
+    if (response) {
+      alert("Successfully updated post");
+    } else {
+      alert("Something went wrong, failed to update post.");
     }
-
+  } catch (error) {
+    console.error("Error updating post:", error);
+  }
 }
