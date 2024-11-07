845669c211cd98254dcf4cbbfa19a43c7f20090a
senbet22 <152603405+senbet22@users.noreply.github.com>
2024-11-07 15:27:23 +0700

    update logout

--- src/js/ui/global/logout.js
+++ src/js/ui/global/logout.js
@@ -1,4 +1,12 @@
 /**
  * Functions you attach to logout events that calls ui/auth/logout function
  */
-export function setLogoutListener() {}
+export function setLogoutListener() {
+  const logoutButton = document.getElementById("logout-button");
+
+  if (logoutButton) {
+    logoutButton.addEventListener("click", onLogout);
+  } else {
+    console.warn("Logout button not found.");
+  }
+}
