67904aef04a940d46528a0e6e52f8561690047bd
senbet22 <152603405+senbet22@users.noreply.github.com>
2024-11-08 21:10:59 +0700

    local.Storage set-item

--- src/js/api/auth/login.js
+++ src/js/api/auth/login.js
@@ -10,6 +10,8 @@
 
 import { API_AUTH_LOGIN } from "../constants";
 
+const API_KEY = "4642c20d-b0b6-45dd-85a2-4e43bf57c602";
+
 export async function login({ email, password }) {
   try {
     const response = await fetch(API_AUTH_LOGIN, {
@@ -22,11 +24,19 @@ export async function login({ email, password }) {
 
     if (!response.ok) {
       const errorData = await response.json();
-      throw new Error(`${errorData.statusCode}: ${errorData.status}. ${errorData.errors[0].message}`);
+      throw new Error(
+        `${errorData.statusCode}: ${errorData.status}. ${errorData.errors[0].message}`
+      );
     }
 
     const data = await response.json();
+
+    localStorage.setItem("token", data.data.accessToken);
+    localStorage.setItem("apiKey", API_KEY);
+    localStorage.setItem("adminUser", JSON.stringify(data.data));
+
     console.log("Login successful:", data);
+
     return data;
   } catch (error) {
     throw error;
