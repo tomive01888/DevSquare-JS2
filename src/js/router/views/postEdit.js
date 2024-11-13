73b42d91e031241da72d6afb117f0411de8fba10
tomive_NOROFF <152199718+tomive01888@users.noreply.github.com>
2024-11-13 17:29:42 +0100

    quick fix import causing error to npm build

--- src/js/router/views/postEdit.js
+++ src/js/router/views/postEdit.js
@@ -1,10 +1,10 @@
 import { authGuard } from "../../utilities/authGuard";
 import { onUpdatePost } from "../../ui/post/update";
 import { readPost } from "../../api/post/read";
-import { populateEditForm } from "../../ui/post/populateEditForm";
 import { setLogoutListener } from "../../ui/global/logout";
 import { goToProfilePage } from "../../ui/global/goMyProfile";
 import { onDeletePost } from "../../ui/post/delete";
+import { populateEditForm } from "../../ui/component/populateEditForm";
 
 authGuard();
 setLogoutListener();
@@ -17,7 +17,6 @@ const urlSearch = new URLSearchParams(window.location.search);
 const postId = urlSearch.get("post");
 
 const postData = await readPost(postId);
-
 await populateEditForm(postData);
 
 document.querySelector(".delete-post").addEventListener("click", onDeletePost);
