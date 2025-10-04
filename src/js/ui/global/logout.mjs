c68ff02601c97ac5f6f82ba625a1160c23f82f60
tomive_NOROFF <152199718+tomive01888@users.noreply.github.com>
2025-10-04 10:46:14 +0200

    feat: Resolve faulty logout button and add .env.example

--- src/js/ui/global/logout.mjs
+++ src/js/ui/global/logout.mjs
@@ -3,8 +3,6 @@ import { onLogout } from "../auth/logout.mjs";
  * Functions you attach to logout events that calls ui/auth/logout function
  */
 export function setLogoutListener() {
-  document.addEventListener("DOMContentLoaded", () => {
-    const logoutButton = document.getElementById("logout-button");
-    logoutButton.addEventListener("click", onLogout);
-  });
+  const logoutButton = document.getElementById("logout-button");
+  logoutButton.addEventListener("click", onLogout);
 }
